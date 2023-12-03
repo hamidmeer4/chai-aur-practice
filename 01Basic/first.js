1380c6b83ca3374960628acddd62909a868794aa
Hamid Meer <103202419+hamidmeer4@users.noreply.github.com>
2023-12-03 15:29:42 +0000

    first, cover a bit of variable

--- 01Basic/first.js
+++ 01Basic/first.js
@@ -1,1 +1,11 @@
-console.log("Hamid")
+const userId = 123;
+let userName = 'Hamid';
+var clientID  = '123345777';
+
+// userId = '12345'
+userName = 'hamid meer';
+clientID  = '7654421'
+
+// console.table([userId,userName,clientID])
+console.log(userId)
+console.log(clientID)
