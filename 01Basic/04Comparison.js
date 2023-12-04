6769d2c83b69f09fbe3ef25eae99ea268abed21b
Hamid Meer <103202419+hamidmeer4@users.noreply.github.com>
2023-12-04 11:32:14 +0000

    why is string to number conversion is confusing

---
+++ 01Basic/04Comparison.js
@@ -0,0 +1,47 @@
+// Basic Operator 
+
+// console.log(2 + 4)
+// console.log(2 - 4)
+// console.log(2 % 4)
+// console.log(2 / 4)
+// console.log(2 * 4)
+// console.log(2 ** 4)
+
+
+
+// console.log('2' + 1)
+// console.log('02' + 1)
+// console.log('4' + '4')
+// console.log('2' + 2 + 5)
+// console.log( 2 + 5 + '3')
+
+
+// tricky 
+
+// console.log(true)
+// console.log(+true)
+// console.log(false)
+// console.log(+false)
+
+// console.log(+""); // 0 
+
+
+// not readible code 
+
+
+// let num1 , num2 ,num3 ;
+
+// num1 = num2 = num3 = 32 + 43;
+
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+
+
+
+
+
+
+
+
+
