86a785fe31712f826f99671d0dfdf81238749daa
Hamid Meer <103202419+hamidmeer4@users.noreply.github.com>
2023-12-04 10:22:49 +0000

    Conversion of DataType

---
+++ 01Basic/03ConversionDataType.js
@@ -0,0 +1,49 @@
+
+// Investigation 
+
+
+let num = 123;
+// console.log(num)
+
+// Numer convert to string 
+// let numString = String(num)
+// console.log(numString);
+// console.log(typeof numString);
+
+// string convert to number 
+
+// let stringNum = '4324532';
+// let stringConver = Number(stringNum)
+
+// console.log(stringNum);
+// console.log(typeof stringConver);
+
+
+// Boolean conversion
+
+let boolean1 = true // try false
+console.log(boolean1)
+
+// string 
+let booleanConver = String(boolean1)
+console.log(typeof booleanConver)
+
+// Number
+let booleanConver2 = Number(boolean1)
+console.log(booleanConver2); 
+
+
+// number to convert boolean 
+
+let booleanNum = 0;
+let booleanNumConver = Boolean(booleanNum)
+
+console.log(booleanNumConver);
+console.log(typeof booleanNumConver);
+
+
+let stringBool = '1';
+let stringBoolNum = Boolean(stringBool)
+
+console.log(stringBoolNum);
+console.log(typeof stringBoolNum);
