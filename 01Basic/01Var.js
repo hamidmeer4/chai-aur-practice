a43f29823e38fc6cdb202a4835a90e0ca8487934
Hamid Meer <103202419+hamidmeer4@users.noreply.github.com>
2023-12-05 06:46:03 +0000

    we practice basic var

---
+++ 01Basic/01Var.js
@@ -0,0 +1,11 @@
+let userName = 'Hamid';
+var clientID  = '123345777';
+
+
+userName = 'hamid meer';
+clientID  = '7654421'
+
+// console.table([userId,userName,clientID])
+
+console.log(clientID)
+
