f5463ac18bf262c65f2400b1d0f713a6688fcfbf
Hamid Meer <103202419+hamidmeer4@users.noreply.github.com>
2023-12-05 14:30:30 +0000

    we create constructor method in string

---
+++ 01Basic/06stringMethod.js
@@ -0,0 +1,21 @@
+
+// String 
+
+// string interpolation  > backtick  > ``
+
+// string is object > The String() constructor creates String objects. When called as a function, it returns primitive values of type String.
+
+// const a = new String("Hello world"); // a === "Hello world" is false
+// const b = String("Hello world"); // b === "Hello world" is true
+
+// console.log(a === 'Hello world')
+
+// console.log(b === 'Hello world')
+
+// console.log(a instanceof String); // is true
+// console.log(b instanceof String); // is false
+
+// console.log(typeof a); // "object"
+// console.log(typeof b); // "string"
+
+
