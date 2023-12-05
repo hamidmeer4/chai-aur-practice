440979ba7165034d93c7a78f2ebd583caa62b4ec
Hamid Meer <103202419+hamidmeer4@users.noreply.github.com>
2023-12-05 06:44:34 +0000

    we have explain prem and non-prem and  practice

---
+++ 01Basic/05SummaryDataType.js
@@ -0,0 +1,126 @@
+
+// Primitive and non Primitive 
+
+// Primitive : Call By Value > enka original value change nahi hota just copy value change 
+// example
+ 
+// let value1 = 123;
+// let value2 = value1;
+
+// value2 = 54311;
+
+// console.log(value1);
+// console.log(value2);
+
+
+// Now we Cover all of primitive data type 
+// there are 7 primitive data type 
+
+// String
+
+// let name = 'hamid';
+// let string2 = name;
+// string2 = 'meer'
+
+// console.log(typeof string);
+// console.log(name);
+// console.log(string2);
+
+
+// ii) Number 
+
+// let value1 = 123;
+// let value2 = value1;
+
+// value2 = 54311;
+
+// console.log(typeof value1);
+// console.log(value1);
+// console.log(value2);
+
+
+
+// iii) Boolean 
+
+
+// let bool1 = true;
+// let bool2 = bool1;
+
+// bool2 = 54311;
+
+// console.log(typeof bool1);
+// console.log(bool1);
+// console.log(bool2);
+
+
+
+// BigInt 
+
+
+// Symbol > any value is unique 
+
+// let id = Symbol('12')
+// let id2 = Symbol('12') 
+
+// console.log(id === id2);
+
+
+// Null :
+// let id = null ;
+// let id2 = id; 
+// id2 = 'hamid';
+
+// console.log(typeof id)
+// console.log(id)
+// console.log(id2)
+
+
+
+// undefined : 
+
+// let id = undefined;
+
+// let id2 = id;
+
+// id2 = 'hahhaha';
+
+// console.log(typeof id)
+// console.log(id)
+// console.log(id2)
+
+
+// javascript is dynamically language
+
+// Non Premitive > jo bhe changes hoga wo original main he hoga 
+
+// there are 3 non premitive 
+
+
+
+// array , object ,function 
+
+// array : 
+
+// let arr = ['hamid','meer',12]
+
+// let arr2 = arr;
+
+// arr2[1] = 'ullah';
+
+// console.log(typeof arr);
+// console.log(arr);
+// console.log(arr2);
+
+
+// let obj = {
+//     name : 'Haid',
+//     age : 23,
+//     nick : 'meer',
+// }
+
+// let obj2 = obj;
+// obj2.age = 20;
+
+// console.log(typeof obj)
+// console.log(obj)
+// console.log(obj2)
