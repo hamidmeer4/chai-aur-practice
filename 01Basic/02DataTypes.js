8e66fe902d126ee96415341f5e23460c54c70cef
Hamid Meer <103202419+hamidmeer4@users.noreply.github.com>
2023-12-05 06:46:38 +0000

    we discuss datatypes in js

---
+++ 01Basic/02DataTypes.js
@@ -0,0 +1,28 @@
+"use Strict" // treat all js code as a newer version 
+
+// alert (4 + 9) // we are using node js ,not browser esliye ye hamein error de raha hai first time js browser based language tha abhi ye backend pay bhe karta hai or hum abhi outside the browser use karty hai 
+
+
+console.log(87 +  
+    65)   // code readibilty should be high  : code readlibilty ka khaas rakna hoga mtlb hamein sahi samjh ajaye code k 
+
+
+
+    
+    // DataTypes : 
+
+    // Number : 2 to powe 53 
+
+    // BigInt : jab apka number bara ho then we use BigInt > like StockMarket or Trading App 
+
+    // String : Double Quotes or Single Quotes
+
+    // Boolean : true / false ;
+
+    // null : StandAlone VAlue > typeof > Object 
+
+    // undefined : the value not be assigned yet 
+
+    // Symbol > unique Value 
+
+    console.log(typeof null);
