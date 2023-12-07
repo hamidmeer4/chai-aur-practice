746c01b5d2c6111ad9dcba91d86405f17a735242
Hamid Meer <103202419+hamidmeer4@users.noreply.github.com>
2023-12-07 20:22:39 +0000

    we practice in math and number mthod

---
+++ 01Basic/07MathNumber.js
@@ -0,0 +1,54 @@
+// const num = 233;
+// toString
+// console.log(num.toString());
+// console.log(num.toString().concat(3332));
+
+// to fixed 
+// console.log(num.toFixed(2))
+
+
+// toLocaleString
+// Create a number to format
+// const numberToFormat = 1234567.89;
+
+// // Format the number using the Indian locale
+// const formattedNumber = numberToFormat.toLocaleString('en-IN');
+
+// // Display the result
+// console.log(formattedNumber);
+
+
+
+// console.log(num.toPrecision(6));
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++ Math +++++++++++++++++++++++++++++++++++++++
+
+
+// const mathNumb = 123;
+// const num2 = Math.abs(mathNumb)
+// 
+// ABS
+// console.log(Math.abs(-45));
+
+// console.log(Math.ceil(4.3));
+
+// console.log(Math.exp(2,2));
+
+
+// console.log(Math.floor(4.7));
+
+
+// console.log(Math.pow(34,2));
+
+
+console.log(Math.min(24,2,4,65,34))
+console.log(Math.max(24,2,4,65,34))
+console.log(Math.floor(Math.random()*10));
+
+
+
+
+
